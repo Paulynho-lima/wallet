431dd30ec217dcbb9916bc3299640e162ec86437
Paulo Lima <paulynho_lima@hotmail.com>
2022-10-20 16:53:28 -0300

    fix add variavel em BrowserRouter

--- src/index.js
+++ src/index.js
@@ -1,19 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
-import { Provider } from 'react-redux';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
-import store from './store/index';
+/* eslint-disable comma-dangle */
+/* eslint-disable quotes */
+/* eslint-disable react/jsx-curly-spacing */
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+import store from "./store/index";
 
 ReactDOM.render(
-  <BrowserRouter>
-    <Provider store={ store }>
+  <BrowserRouter basename={process.env.PUBLIC_URL}>
+    <Provider store={store}>
       <App />
     </Provider>
   </BrowserRouter>,
-  document.getElementById('root'),
+  document.getElementById("root")
 );
 
 // If you want your app to work offline and load faster, you can change
